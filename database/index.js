32ca972ea5db0be4ddd6bcde4b3d4c4f4e3c9c7f
rebbyv <rebsvincent@gmail.com>
2020-07-28 21:29:10 -0500

    Draft CRUD routes & mysql connection setup

---
+++ database/index.js
@@ -0,0 +1,13 @@
+var mysql = require('mysql');
+var dbCreds = require('../config.js');
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: dbCreds.mysqlUser,
+  password: dbCreds.mysqlPassword,
+  database: 'ESS'
+})
+
+connection.connect((error) => {
+  error ? console.log(`Error connecting to mysql: ${error.stack}`): console.log('Connected to mysql');
+})
--- server/index.js
+++ server/index.js
@@ -2,7 +2,33 @@ const express = require('express');
 const port = process.env.PORT || 2000;
 const app = express();
 
+// load client portal
 app.use(express.static('client/dist'))
+
+
+// Create
+app.post('/course/:type', (req, res) => {
+  // type will be course or name
+})
+
+// Read
+app.get('/course/:type/:query', (req, res) => {
+  // type will be courseId, courseName, or testName
+  // query is the search entered by user
+})
+
+// Update
+app.put('/course/:type/:id', (req, res) => {
+  // type will be course or test
+  // id is the course or test to update
+})
+
+// Delete
+app.delete('/course/:type/:id', (req, res) => {
+  // type will be course or test
+  // name will be the name of the course or test
+})
+
 
 app.listen(port, (error) => {
   error ? console.log('Error in establing server connection'): console.log(`Express server listening on port: ${port}`);
